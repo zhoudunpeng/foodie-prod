cde64ee537da5055793d1a6f2194c0ec6628b6e1
zhoudunpeng <zhoudunpeng@canpoint.com>
2021-05-19 17:07:07 +0800

    整合支付模块

---
+++ food-prod-api/src/main/java/com/zdp/controller/center/CenterUserController.java
@@ -0,0 +1,92 @@
+package com.zdp.controller.center;
+
+
+import com.zdp.controller.BaseController;
+import com.zdp.pojo.Users;
+import com.zdp.pojo.bo.center.CenterUserBO;
+import com.zdp.service.center.CenterUserService;
+import com.zdp.utils.CookieUtils;
+import com.zdp.utils.DateUtil;
+import com.zdp.utils.IMOOCJSONResult;
+import com.zdp.utils.JsonUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.util.http.fileupload.FileUpload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Api(value = "用户信息接口", tags = {"用户信息相关接口"})
+@RestController
+@RequestMapping("userInfo")
+public class CenterUserController extends BaseController {
+
+    @Autowired
+    private CenterUserService centerUserService;
+
+
+    @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
+    @PostMapping("update")
+    public IMOOCJSONResult update(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println(centerUserBO);
+
+        // 判断BindingResult是否保存错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
+
+        Users userResult = centerUserService.updateUserInfo(userId, centerUserBO);
+
+        userResult = setNullProperty(userResult);
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+        // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
+
+        return IMOOCJSONResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error : errorList) {
+            // 发生验证错误所对应的某一个属性
+            String errorField = error.getField();
+            // 验证错误的信息
+            String errorMsg = error.getDefaultMessage();
+
+            map.put(errorField, errorMsg);
+        }
+        return map;
+    }
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setPassword(null);
+        userResult.setMobile(null);
+        userResult.setEmail(null);
+        userResult.setCreatedTime(null);
+        userResult.setUpdatedTime(null);
+        userResult.setBirthday(null);
+        return userResult;
+    }
+
+}
